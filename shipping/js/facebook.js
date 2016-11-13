0f0700a10ef800e76ef4026463033d703b231267
nLevin13 <nyro9999@gmail.com>
2016-11-13 03:26:25 -0800

    SCREW YOU GIT21

--- shipping/js/facebook.js
+++ shipping/js/facebook.js
@@ -27,11 +27,11 @@
     
     function getUserFriends() {
   FB.api('/{user-id}/friends', function (response) {
-    console.log('Got friends: ', response.data);
+    console.log('Got friends: ', response.friends);
     //$("#friendslist").html('<img src="'+response.data[0].picture.data.url+'"/>');
      //maybe an $.each as well
      var friendMarkup = '';
-     $.each(response.data, function(e, v) {
+     $.each(response.friends, function(e, v) {
        friendMarkup += '<img src="' + v.picture.data.url +'" alt="'+ v.name +' picture"/>';
      })
      $("#friendlist").html(friendMarkup);
