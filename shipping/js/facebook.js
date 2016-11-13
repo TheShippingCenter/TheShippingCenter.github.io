1ee4e48133f5fdb9d453d747811eef5161070d0b
nLevin13 <nyro9999@gmail.com>
2016-11-13 00:43:46 -0800

    SCREW YOU GIT2

--- shipping/js/facebook.js
+++ shipping/js/facebook.js
@@ -7,14 +7,44 @@
       FB.AppEvents.logPageView();
       
      FB.getLoginStatus(function(response) {
+         login();
           if(response.status === 'connected'){
-              //document.getElementById('status').innerHTML = 'We are connected.';
-              alert("YAYAYYAYA");
+              document.getElementById('status').innerHTML = 'We are connected.';
           } else if (response.status === 'not_authorized') {
-             // document.getElementById('status').innerHTML = 'We are not logged in';
-              alert("shit");
+              document.getElementById('status').innerHTML = 'We are not logged in';
           } else {
-             // document.getElementById('status').innerHTML = 'You are not logged into Facebook';
-              alert("shit");
+              document.getElementById('status').innerHTML = 'You are not logged into Facebook';
           }});
   };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+    
+    function login() {
+        FB.login(function(response) {
+            if(response.status === 'connected'){
+              document.getElementById('status').innerHTML = 'We are connected.';
+                
+                
+                console.log(response.first_name);
+                
+                
+          } else if (response.status === 'not_authorized') {
+              document.getElementById('status').innerHTML = 'We are not logged in';
+          } else {
+              document.getElementById('status').innerHTML = 'You are not logged into Facebook';
+          }
+        }
+                );
+    }
+            
+function getFirstName() {
+    console.log(response.first_name);
+}
+    
+
